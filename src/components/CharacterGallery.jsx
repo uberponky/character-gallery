25f3dfb60f14928422d8354bbfaa09c71fae9cf6
Rory Simmonds <rory@blankbox.business>
2024-03-09 11:33:27 +0000

    add

--- src/components/CharacterGallery.jsx
+++ src/components/CharacterGallery.jsx
@@ -5,6 +5,7 @@ const CharacterGallery = () => {
     const listOfChars = characterData.map((char, i) => <Character { ...char } key={ i } />)
     return (
         <section>
+            <h1>An egg of an egg of an egg</h1>
             { listOfChars }
         </section>
     );
